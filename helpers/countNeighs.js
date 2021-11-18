f166bda280cf8ff649e85cab581bbda09c625683
Darren B <68653294+Devd0@users.noreply.github.com>
2021-11-18 12:00:35 -0500

    commit #1-concept of game complete

---
+++ helpers/countNeighs.js
@@ -0,0 +1,62 @@
+// x,y = row, col
+
+// RULES
+    // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    // Any live cell with two or three live neighbours lives on to the next generation. - //
+    // Any live cell with more than three live neighbours dies, as if by overpopulation.
+    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+// array is the main grid - mirror is grid2
+export const countAdjCells = (array, mirror, gridHeight, gridWidth) => {
+
+    for (let x = 1; x < gridHeight - 1; x++) {
+        for (let y = 1; y < gridWidth -1; y++) {
+            let totalneighs = 0;
+            totalneighs += array[x - 1][y - 1];
+            totalneighs += array[x][y - 1];
+            totalneighs += array[x + 1][y - 1];
+            totalneighs += array[x - 1][y];
+            totalneighs += array[x + 1][y];
+            totalneighs += array[x - 1 ][ y + 1 ];
+            totalneighs += array[x][y + 1];
+            totalneighs += array[x + 1][ y + 1];
+
+            // apply rules to each cell
+            if (array[x][y] === 0) {
+                switch(totalneighs) {
+                    case 3:
+                        mirror[x][y] = 1; // if ded cell but has 3 neighs then it gets reproduced
+                        break;
+                    default:
+                        mirror[x][y] = 0; // if not just leave cell ded
+                };
+            } else if(array[x][y] === 1) {
+                switch(totalneighs) {
+                    case 0:
+                    case 1:
+                        mirror[x][y] = 0;
+                        break;
+                    case 2:
+                    case 3:
+                        mirror[x][y] = 1; // keep life
+                        break;
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8: // will only have up to 8 neighs
+                        mirror[x][y] = 0; // die of over-population
+                        break;
+                    default:
+                        mirror[x][y] = 0;
+
+                };
+            };
+        };
+    };
+
+    return mirror
+}; 
+// the cell that 2, 2 is ajd to in top left
+[0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1]
+
---
+++ src/main.js
@@ -0,0 +1,119 @@
+import { countAdjCells } from '../helpers/countNeighs.js'
+
+const startGame = document.getElementById('start-game');
+const randGrid = document.getElementById('gen-rand');
+
+// columns, rows - (THESE VALUESs NEED TO BE THE CANVAS WIDTH X CANVAS HEIGHT)
+// right now for each 1 that we find we render out one (one pixel) square 
+let gridHeight = 80;
+let gridWidth =  80;
+
+
+// create grid
+const createArray = function(rows) {
+    let arr = [];
+
+    for (let i = 0; i < rows; i++) {
+        arr[i] = [];
+    };
+
+    return arr;
+};
+
+
+
+
+
+// Change: Make it such that each array inside the main array is either all 0s or all ones (not a random mix)
+
+const createGridRandomly = function(grid) {
+
+    for (let i = 0; i < gridHeight; i++) {
+        
+        for (let j = 0; j < gridWidth; j++) {
+            const randOneZero = Math.floor(Math.random() * 2);
+            // gets me a random zero or 1 to append to array
+            if (randOneZero === 1) {
+                grid[i][j] = 1;
+            } else {
+                grid[i][j] = 0;
+            }; 
+        };
+    };
+}; 
+
+
+
+// 80 x 80 (80 rows of 80 cells)
+const drawGrid = function(grid) {
+    const mainCanvas = document.querySelector('canvas');
+    const context = mainCanvas.getContext('2d');
+    context.clearRect(0, 0, 800, 800);
+    let countery = 0;
+    let counterx = 0;
+    for (let i = 0; countery < gridHeight; i += 10) {
+        
+        counterx = 0;
+        for (let j = 0; counterx < gridWidth; j += 10) {
+            if (grid[countery][counterx] === 1) {
+                context.fillStyle = '#FF0000';
+                context.fillRect(j, i, 10, 10);
+            };
+            
+            counterx += 1; 
+        };
+        countery += 1;
+    }; 
+};
+
+
+
+
+
+
+// 80 x 80 - 80 rows of 10 pixel cells
+const showRandTemp = function() {
+    let grid = createArray(gridWidth);
+    createGridRandomly(grid);
+    drawGrid(grid);
+    
+    return grid;
+};
+
+
+
+let grid2 = createArray(gridWidth);
+
+let grid = showRandTemp()
+const playGame = function() {
+    drawGrid(grid);
+    grid2 = countAdjCells(grid, grid2, gridHeight, gridWidth);
+
+    for (let i = 0; i < gridHeight; i++) {
+        for (let j = 0; j < gridWidth; j++) {
+            grid[i][j] = grid2[i][j];
+        };
+    };
+
+    requestAnimationFrame(playGame)
+};
+
+
+
+startGame.addEventListener('click', playGame);
+randGrid.addEventListener('click', () => {
+    location.reload()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
